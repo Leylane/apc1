74d44cd7a287982719cbd9e4e8ffe89c9f5856ea
Leylane <30903431+Leylane@users.noreply.github.com>
2024-09-05 02:40:45 +0000

    aula4

---
+++ Aulas/aula04/ler_caracteres.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(){
+  char tecla = '\0'; //caractere nulo
+
+  printf("Digite uma tecla: ");
+  int deu_certo = scanf("%c", &tecla);
+  getchar(); //limpa o \n
+  printf("Você digitou: %c\n", tecla);
+
+  printf("Digite outra tecla: ");
+  deu_certo = scanf("%c", &tecla);
+  getchar();
+  printf("Você digitou: %c\n", tecla);
+
+  char nome[31];
+  printf("Digite seu nome: ");
+  deu_certo = scanf("%[^\n]s", &nome);// as chaves procuram o \n no final do texto
+  printf("Olá %s!\n", nome); 
+
+  return 0;
+}
---
+++ praticas/pratica04/leia_decimais.c
@@ -0,0 +1,11 @@
+#include <stdio.h>;
+
+int main(){
+  int float;
+  printf("Digite um preço");
+  scanf("%f", &preco);
+
+  printf("Você digitou %.2f\n", preco);
+
+  return 0;
+}
